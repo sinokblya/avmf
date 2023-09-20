1e9dd6aae7f066113410af8b651355cb2b1e4764
sinokblya <115476497+sinokblya@users.noreply.github.com>
2023-09-20 10:24:03 +0500

    Add files via upload

---
+++ lab1/NamedPipeClient/Console Application/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
+public struct UserInfo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public UserInfo(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+
+
+namespace NamedPipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "MyPipe", PipeDirection.InOut))
+            {
+                Console.WriteLine("Подключение к серверу...");
+                pipeClient.Connect();
+
+                Console.Write("Введите имя: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Введите возраст: ");
+                int age = int.Parse(Console.ReadLine());
+
+                UserInfo user = new UserInfo { Name = name, Age = age };
+                string jsonUserData = JsonSerializer.Serialize(user);
+                byte[] buffer = Encoding.UTF8.GetBytes(jsonUserData);
+                pipeClient.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("Данные отправлены серверу.");
+
+                buffer = new byte[1024];
+                int bytesRead = pipeClient.Read(buffer, 0, buffer.Length);
+                string jsonResponse = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                UserInfo responseUser = JsonSerializer.Deserialize<UserInfo>(jsonResponse);
+                Console.WriteLine($"Ответ от сервера: Имя: {responseUser.Name}, Возраст: {responseUser.Age}");
+
+                pipeClient.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab1/NamedPipeServer/Console Application/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
+public struct UserInfo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public UserInfo(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+
+namespace NamedPipeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("MyPipe", PipeDirection.InOut))
+            {
+                Console.WriteLine("Ожидание подключения клиента...");
+                pipeServer.WaitForConnection();
+
+                Console.WriteLine("Клиент подключен.");
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = pipeServer.Read(buffer, 0, buffer.Length);
+                string jsonUserData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                UserInfo receivedUser = JsonSerializer.Deserialize<UserInfo>(jsonUserData);
+
+                Console.WriteLine($"Получены данные от клиента: Имя: {receivedUser.Name}, Возраст: {receivedUser.Age}");
+
+                UserInfo responseUser = new UserInfo();
+                string jsonResponse = JsonSerializer.Serialize(responseUser);
+                byte[] responseBuffer = Encoding.UTF8.GetBytes(jsonResponse);
+                pipeServer.Write(responseBuffer, 0, responseBuffer.Length);
+                Console.WriteLine("Ответ отправлен клиенту.");
+
+                pipeServer.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
